0c109e9d0a824d9ea539632a6a4f32374c4a8f90
tylerjustice <24rules@windstream.net>
2020-08-07 02:05:31 +0000

    completed cards

---
+++ assets/js/modal.js
@@ -0,0 +1,76 @@
+document.getElementById('button').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+});
+
+document.getElementById('hamburger').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+});
+
+document.querySelector('.button').addEventListener('click',
+function() {
+    document.querySelector('.content').style.display = 'none';
+});
+
+document.getElementById('button').addEventListener('click',
+function() {
+    document.querySelector('.content').style.display = 'block';
+});
+
+document.querySelector('.button').addEventListener('click',
+function() {
+    document.querySelector('.sub-sent').style.display = 'block';
+});
+
+document.querySelector('.button').addEventListener('click',
+function() {
+    document.querySelector('.sub-sent-content').style.display = 'block';
+});
+
+document.querySelector('.close').addEventListener('click',
+function() {
+    document.querySelector('.sub-sent-content').style.display = 'none';
+});
+
+document.querySelector('.close').addEventListener('click',
+function() {
+    document.querySelector('.sub-sent').style.display = 'none';
+});
+
+document.querySelector('.button').addEventListener('click',
+function() {
+    document.querySelector('.btn-close').style.display = 'inline-block';
+});
+
+document.querySelector('.close').addEventListener('click',
+function() {
+    document.querySelector('.btn-close').style.display = 'none';
+});
+
+document.querySelector('.btn-close').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+});
+
+document.querySelector('.btn-close').addEventListener('click',
+function() {
+    document.querySelector('.btn-close').style.display = 'none';
+});
+
+document.querySelector('.btn-close').addEventListener('click',
+function() {
+    document.querySelector('.sub-sent-content').style.display = 'none';
+});
+
+document.querySelector('.btn-close').addEventListener('click',
+function() {
+    document.querySelector('.sub-sent').style.display = 'none';
+});
+
+
