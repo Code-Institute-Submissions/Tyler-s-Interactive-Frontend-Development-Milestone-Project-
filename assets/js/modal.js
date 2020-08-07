feeeb65b69811b19c9601a94b55e0c42baac4a1f
tylerjustice <24rules@windstream.net>
2020-08-07 18:10:36 +0000

    finished project without ReadME

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -73,4 +73,73 @@ function() {
     document.querySelector('.sub-sent').style.display = 'none';
 });
 
+function checkforblank() {
+
+    if (document.querySelector('.names').value == "") {
+        alert('Please enter your name');
+        return false;
+    }
+    if (document.querySelector('.emails').value == "") {
+        alert('Please enter your email');
+        return false;
+    }
+    if (document.querySelector('.text-area').value == "") {
+        alert('Please enter your project description');
+        return false;
+    }
+}
+
+document.querySelector('.card-1-btn').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal-2').style.display = 'flex';
+});
+
+document.querySelector('.close-2').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal-2').style.display = 'none';
+});
+
+document.querySelector('.btn-close-2').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal-2').style.display = 'none';
+});
+
+
+document.querySelector('.card-2-btn').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal-3').style.display = 'flex';
+});
+
+document.querySelector('.close-3').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal-3').style.display = 'none';
+});
+
+document.querySelector('.btn-close-3').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal-3').style.display = 'none';
+});
+
+
+document.querySelector('.card-3-btn').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal-4').style.display = 'flex';
+});
+
+document.querySelector('.close-4').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal-4').style.display = 'none';
+});
+
+document.querySelector('.btn-close-4').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal-4').style.display = 'none';
+});
+
+
+
+
+
+
+
 
